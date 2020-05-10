6bbdd2728aabd164d0b47880b7901308c04b5c29
uron <deazx.uron2019@gmail.com>
2020-05-10 10:01:20 +0900

    feat: transfer the data to github

---
+++ discordbot.py
@@ -0,0 +1,488 @@
+#coding:utf-8
+from pix import prkn_img
+from pix.tesseract import tesseract
+import requests
+import discord
+import re
+from sprd import spreadsheet
+import json
+
+# read token
+TOKEN_FILE=open("./token","r")
+TOKEN=TOKEN_FILE.read().replace('\n','')
+
+text_channel=None
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('')
+
+def message_start_comp(msg):
+    if msg.startswith('/おはよう'):
+        return True
+    if msg == '/kokkoro':
+        return True
+    return False
+
+def message_fin_comp(msg):
+    if msg.startswith('/おやすみ'):
+        return True
+    if msg == '/kokkoro sleep':
+        return True
+    return False
+
+async def show_help(message):
+    await message.channel.send('[/kokkoro] コッコロBotが起動します')
+    await message.channel.send('[/kokkoro sleep] コッコロBotが停止します')
+
+async def show_stat(message):
+    global text_channel
+    if text_channel == None:
+        await message.channel.send("主さま、読み上げは行っておりませんよ")
+        return
+    await message.channel.send("主さま、"+text_channel.name+"でございます")
+
+def download_img(url, file_name):
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(file_name, 'wb') as f:
+            f.write(r.content)
+
+def member_search(message,name):
+    for m in message.guild.members:
+        if m.name.find(name) != -1:
+            return m
+        if m.nick is not None and m.nick.find(name) != -1:
+            return m
+    return None
+
+#botに対してmentionがあったか
+#またworldが含まれているか
+def check_mention(message,word):
+    global client
+    my_mention = False
+    for user in message.mentions:
+        if user == client.user: 
+            my_mention = True
+
+    if my_mention == False:
+        return None
+    # <>で囲まれていて自信に充てられていたら反応する
+    find = message.content.split(">")
+    if len(find) < 2:
+        print("error not split "+message.content)
+        return None
+    # 文字数少なすぎ
+    if len(find[1]) < 2:
+        return None
+
+    return find[1] if word in find[1] else None
+
+async def boss_mention(message,boss_name):
+    print("boss_mention:"+message.content)
+    userlist = spreadsheet.next_attack_member(boss_name)
+    if userlist is None:
+        await message.channel.send("sheetがありませんよ。主さま、名前の確認をしてくださいね")
+        return False
+    await message.channel.send("ともあれ"+boss_name+"を討伐するのですね")
+    #await message.channel.send("@everyone "+boss_name+"に入りましたよ")
+    if len(userlist) < 1:
+        await message.channel.send("予約している方が1人も見つかりませんでしたよ")
+        return False
+    # name,damage,ra
+    t = userlist[0].split(",")
+    await message.channel.send(t[2]+"周目の討伐です")
+    total_damage = 0
+    for u in userlist:
+        user_datas = u.split(",")
+        name=user_datas[0]
+        total_damage+=int(user_datas[1])
+        member=member_search(message,name)
+        if member is None:
+            await message.channel.send("はて?"+name+"さまが見つかりませんね。お名前を変更なさったのでしょうか？")
+            continue
+        reply = f'{member.mention}'
+        await message.channel.send(reply+"さま出番ですよ。頑張ってくださいまし")
+    await message.channel.send("トータルで"+str(total_damage)+"万のダメージ量になるようです！")
+    return True
+
+# messageには[予約]等が期待される
+def check_reserve_chennel(message):
+    # boss_chennelか
+    if message.channel is None:
+        return None
+
+    boss_ch_pattern = message.channel.name
+    if "boss_" not in boss_ch_pattern:
+        return None
+       
+    boss_name = boss_ch_pattern.split("boss_")[1]
+    if "予約" not in message.content:
+        return None
+
+    # 予約1人目とか入れるのはなしにする？
+    damage = re.sub(r'\D', '',message.content) # d only
+    # ダメージ0とか?
+    if len(damage) < 2:
+        return None
+
+    member = message.author
+    name = member.name
+    if member.nick is not None:
+        name = member.nick
+
+    return boss_name+","+name+","+damage
+
+# 予約を行う
+async def reserve_attacker(message,reserve_bundle):
+    rb = reserve_bundle.split(",")
+    boss_name =rb[0]
+    user_name =rb[1]
+    damage =rb[2]
+    await message.channel.send(boss_name+"に予約でございますね")
+    # 予約処理
+    result = spreadsheet.reserve_attack_member(boss_name,user_name,damage)
+    if result == -1:
+        await message.channel.send("sheetがないです・・・")
+        return
+    elif result == -2:
+        await message.channel.send(user_name+"という方はおりませんでした。")
+        return
+    elif result == -3:
+        await message.channel.send("すみません、主さま満員でございます。(bossのhpを超えている) ")
+        await message.channel.send("周回をまたぐ場合は手動にてご予約をお願いいたします")
+        await message.channel.send("下記の主さまがご予約中でございます")
+        result = spreadsheet.next_attack_member(boss_name)
+    else:
+        await message.channel.send("予約が出来ました。下記の主さまがご予約中です")
+
+    for user in result:
+        ud = user.split(",")
+        name=ud[0]
+        damage=ud[1]
+        ra=ud[2]
+        await message.channel.send(name+"さま "+damage+"万 "+ra+"周")
+
+# messageには[予約中止]等が期待される
+def check_reserve_cancel_chennel(message):
+    # boss_chennelか
+    if message.channel is None:
+        return None
+
+    boss_ch_pattern = message.channel.name
+    if "boss_" not in boss_ch_pattern:
+        return None
+       
+    boss_name = boss_ch_pattern.split("boss_")[1]
+    if "予約中止" not in message.content:
+        return None
+
+    member = message.author
+    name = member.name
+    if member.nick is not None:
+        name = member.nick
+    return boss_name+","+name
+
+# 予約をcancel
+async def reserve_cancel_attacker(message,reserve_bundle):
+    rb = reserve_bundle.split(",")
+    boss_name =rb[0]
+    user_name =rb[1]
+    await message.channel.send(boss_name+"から予約中止でございますね")
+    # 予約中止処理
+    result = spreadsheet.cancel_attack_member(boss_name,user_name)
+    if result == -1:
+        await message.channel.send("sheetが見つかりませんでした・・・お名前の確認をお願いします")
+        return
+    elif result == -2:
+        await message.channel.send(user_name+"という方はおりませんでした。")
+        return
+    elif result == -3:
+        await message.channel.send(user_name+"さまはご登録なされていないようです")
+    else:
+        await message.channel.send("下記の主さまがご予約中です")
+
+        
+    result = spreadsheet.next_attack_member(boss_name)
+    for user in result:
+        ud = user.split(",")
+        name=ud[0]
+        damage=ud[1]
+        ra=ud[2]
+        await message.channel.send(name+"さま "+damage+"万 "+ra+"周")
+
+# =================================================================
+# おわり処理　
+# =================================================================
+# messageには[おわり]or[おわりPT]が期待される
+def check_reserve_finish_chennel(message):
+    # boss_chennelか
+    if message.channel is None:
+        return None
+
+    boss_ch_pattern = message.channel.name
+    if "boss_" not in boss_ch_pattern:
+        return None
+    boss_name = boss_ch_pattern.split("boss_")[1]
+
+    fin = 0
+    pt_num = -1
+    if "おわり" == message.content:
+        fin = 1
+    elif message.content.find("おわり") != -1:
+        pt_data = message.content.split("おわり")[1]
+        if pt_data.find("PT") != -1:
+            pt_num = pt_data.split("PT")[1]
+        if pt_data.find("pt") != -1:
+            pt_num = pt_data.split("pt")[1]
+        if pt_data.find("Pt") != -1:
+            pt_num = pt_data.split("Pt")[1]
+        # pt numが1-3か確認
+        ipt_num = int(pt_num)
+        if (1 <= ipt_num and ipt_num <= 3):
+            fin = 2
+        else:
+            fin = 0
+    else:
+        fin = 0
+
+    # 一致なし
+    if fin == 0:
+        return None
+
+    member = message.author
+    name = member.name
+    if member.nick is not None:
+        name = member.nick
+    return boss_name+","+name+","+str(pt_num)
+
+# 予約を遂行した
+async def reserve_finish_attacker_internal(message,bundle):
+    rb = bundle.split(",")
+    boss_name =rb[0]
+    user_name =rb[1]
+    pt_num =rb[2]
+    await message.channel.send(boss_name+"への攻撃を完了したのですね")
+    # 予約削除処理
+    result = spreadsheet.cancel_attack_member(boss_name,user_name)
+    if result < 0:
+        return result
+    else:
+        # 正常終了の際はPTの確認をする
+        if( int(pt_num) == -1 ):
+            await message.channel.send(user_name+"さま凸記入おわすれになりませんよう、ご注意くださいませ。")
+        else:
+            result = spreadsheet.upd_pt_convex(user_name,pt_num)
+            if result < 0:
+                return result
+            await message.channel.send(user_name+"さまお疲れ様でございます。PT"+str(result)+"に記入しました")
+        return 0
+    
+# 予約を遂行した
+async def reserve_finish_attacker(message,bundle):
+    rb = bundle.split(",")
+    boss_name =rb[0]
+    user_name =rb[1]
+    result = await reserve_finish_attacker_internal(message,bundle)
+    if result == -1:
+        await message.channel.send("sheetが見つかりませんでした・・・お名前の確認をお願いします")
+        return
+    elif result == -2:
+        await message.channel.send(user_name+"という方はおりませんでした。")
+        return
+    elif result == -3:
+        await message.channel.send("主さまはご登録なされていないようです")
+
+    
+    next_atk = spreadsheet.next_attack_member(boss_name)
+    if len(next_atk) <= 0:
+        return
+    await message.channel.send("残りは下記の主さまがいらっしゃいます")
+    for user in next_atk:
+        ud = user.split(",")
+        name=ud[0]
+        damage=ud[1]
+        ra=ud[2]
+        await message.channel.send(name+"さま "+damage+"万 "+ra+"周")
+# =================================================================
+# 討伐完了
+# =================================================================
+# messageには[周目討伐]が期待される
+def check_finish_round_in_bossch(message):
+    # boss_chennelか
+    if message.channel is None:
+        return None
+
+    boss_ch_pattern = message.channel.name
+    if "boss_" not in boss_ch_pattern:
+        return None
+    boss_name = boss_ch_pattern.split("boss_")[1]
+
+    if message.content.find("周目討伐") == -1:
+        return None
+    
+    fin_round = message.content.split("周目討伐")[0]
+    return boss_name+","+fin_round
+
+# 討伐完了した
+async def finish_round_attacker(message,bundle):
+    rb = bundle.split(",")
+    boss_name =rb[0]
+    fin_round =rb[1]
+    await message.channel.send(fin_round+"周目"+boss_name+"の討伐が完了したのですね")
+    # round clear処理
+    result = spreadsheet.clear_round_and_countup(boss_name,fin_round)
+    result_user = result[0]
+    result_round = result[1]
+    if len(result_user) <= 0:
+        await message.channel.send("予約している人はおりませんでした。")
+    else:
+        for user_name in result_user:
+            await message.channel.send(user_name +"さまの予約を消しました ")
+
+    if result_round is not None:
+        await message.channel.send("次は"+str(result_round)+"周目でございます。")
+# =================================================================
+# spred sheet更新
+# =================================================================
+async def update_gss(message):
+    if "/gss" not in message.content:
+        return False
+
+    words = message.content.split("/gss")
+    # url返却
+    if(len(words) <= 1 or len(words[1]) < 2):
+        r=spreadsheet.get_url()
+        await message.channel.send("現在のurlは下記でございます。\n"+r)
+        return True
+
+    if "/gssreload" not in message.content:
+        return False
+
+    r=spreadsheet.reload_url()
+    if r is None:
+        await message.channel.send("urlが変更できませんでした")
+        return True
+    await message.channel.send("urlを下記に変更いたしました\n"+r[1])
+    await message.channel.send("古いurlは下記にございます\n"+r[0])
+    return True
+
+# =================================================================
+# recv message
+# =================================================================
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    global text_channel
+    if message.author.bot:
+        return
+    if message.content == '/help':
+        await show_help(message)
+        return
+    if message.content == '/stat':
+        await show_stat(message)
+        return
+    if message.content == '/test':
+        x = prkn_img.start_template_match("./pr_input2.PNG")
+        await message.channel.send('主さまテストですね')
+        await message.channel.send(x)
+        return
+    # メンバーのリストを取得して表示
+    if message.content == '/memb':
+        await message.channel.send('主さまテストですね')
+        for m in message.guild.members:
+            reply = f'{m.mention}'
+            await message.channel.send(reply)
+        return
+    if await update_gss(message):
+        return
+    if message.content == '/gssxxxxx':
+        download_img("http://drive.google.com/uc?export=view&id=12Cyvx-B_WD43JLOq47e3qEmqeYkteuZK","up.png")
+        await message.channel.send('主さまスプレッドシートのチェックですね')
+        await message.channel.send("http://drive.google.com/uc?export=view&id=12Cyvx-B_WD43JLOq47e3qEmqeYkteuZK")
+        return
+    # 予約キャンセル
+    cancel_bundle=check_reserve_cancel_chennel(message)
+    if cancel_bundle is not None:
+        await reserve_cancel_attacker(message,cancel_bundle)
+        return
+    # 予約finish
+    finish_bundle=check_reserve_finish_chennel(message)
+    if finish_bundle is not None:
+        await reserve_finish_attacker(message,finish_bundle)
+        return
+    #予約メンション
+    reserve_bundle=check_reserve_chennel(message)
+    if reserve_bundle is not None:
+        await reserve_attacker(message,reserve_bundle)
+        return
+    # 討伐完了
+    fr_bundle = check_finish_round_in_bossch(message)
+    if fr_bundle is not None:
+        await finish_round_attacker(message,fr_bundle)
+        return
+    # ボスメンション
+    boss_name=check_mention(message,"")
+    if boss_name is not None:
+        await boss_mention(message,boss_name)
+        return
+
+    # 読み上げ
+    if message_start_comp(message.content):
+        text_channel=message.channel
+        save_text_channel_name(text_channel.name)
+        await text_channel.send('主さま、こちらのチャンネルで入退室の読み上げを開始いたします')
+    if message_fin_comp(message.content):
+        await message.channel.send('主さま、入退室の読み上げを終了いたします')
+        text_channel=None
+
+
+  #  if len(message.attachments) >= 1:
+  #      download_img(message.attachments[0].url,"up.png")
+  #      x = prkn_img.start_template_match("./up.png")
+  #      await message.channel.send("主さま、画像の解析がおわりました")
+  #      await message.channel.send(x)
+
+
+
+def save_text_channel_name(channel_name:str):
+    env_list = spreadsheet.get_env_list()
+    env_list["chat_channel_name"] = channel_name
+    spreadsheet.save_setting_url(env_list)
+
+def update_text_channel():
+    global text_channel
+    channel_name = spreadsheet.get_env_list()["chat_channel_name"]
+    channels = client.get_all_channels()
+    for channel in channels:
+        if channel_name == channel.name:
+            return channel
+    return None
+
+# =================================================================
+# voice chat 
+# =================================================================
+@client.event
+async def on_voice_state_update(member, before, after):
+    global text_channel
+    name = member.name
+    if member.nick is not None:
+        name = member.nick
+
+    if text_channel is None:
+        text_channel = update_text_channel()
+        if text_channel is None:
+            return
+
+    if before.channel == after.channel:
+        return
+    if after.channel is not None:
+        await text_channel.send(name+'さまが'+after.channel.name+'に入室しました')
+    if after.channel is None:
+        await text_channel.send(name+'さまが退室しました')
+
+
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
---
+++ sprd/spreadsheet.py
@@ -0,0 +1,385 @@
+import gspread 
+from oauth2client.service_account import ServiceAccountCredentials
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8') # UTF-8に
+import json
+import os.path
+import re
+
+#================================
+#define
+SETTINGS_URL="https://docs.google.com/spreadsheets/d/1Xxb6OgdY_LqdpV7SfmqyfX_IdSGry0t7-y-sXeHWFLE/edit?usp=sharing"
+SETTINGS_SHEET_NAME="settings"
+SETTINGS_ENV_LIMIT=20 
+WORKBOOK_URL=""
+cell_ROUNDABOUT='B1' #周回数
+cell_BOSS_HP='F2'
+cell_NAMELIST_TOP_BOTTOM='L5:O34' #予約範囲
+cell_USERLIST='B5:B34' #メンバーリスト
+cell_DAMAGELIST='M5:o34' #ダメージリスト
+cell_RESERVELIST='F5:I34' #予約リスト
+PT_CONVEX_SHEET_NAME='凸記入用'
+cell_PT_CONVEX_USERLIST='C3:C32' #メンバーリスト
+
+cell_RANGE_NAME=0
+cell_RANGE_DAMAGE=1
+cell_RANGE_RA=3
+#define
+#================================
+
+#================================
+# global
+path=os.path.dirname(__file__)+"/"
+env_list = dict()
+
+
+#================================
+# private class
+
+class AttackInfo:
+    name = ""
+    damage_str = ""
+    ra_str = ""
+    def __init__(self,_name,_damage,_ra):
+        self.name = _name
+        self.damage_str = _damage
+        self.ra_str = _ra
+
+    def get_string(self):
+        return self.name+","+self.damage_str+","+self.ra_str
+
+    def equal_ra(self,ra_value):
+        # 周回数
+        if len(self.ra_str) < 1 or ra_value != int(self.ra_str):
+            return False
+        return True
+
+#================================
+# private func
+
+def search_sheet(sheet_list,search_name):
+    for w in sheet_list:
+        if w.title == search_name:
+            return w
+    return None
+
+def create_member_list(wks):
+    #メンバー取得
+    cell_list = wks.range(cell_NAMELIST_TOP_BOTTOM)
+    c=4
+    r_member_list = []
+    for i in range(0,20):
+        name = cell_list[cell_RANGE_NAME+i*c].value
+        damage = cell_list[cell_RANGE_DAMAGE+i*c].value
+        ra = cell_list[cell_RANGE_RA+i*c].value
+        if name == "":
+            continue
+        ra=re.sub(r'\D', '',ra) # d only
+        r_member_list.append(AttackInfo(name,damage,ra))
+    return r_member_list
+
+# 予約ダメージ合計
+def calc_total_reserve_damage(wks,ra_value):
+    member_list = create_member_list(wks)
+    total = 0
+    for member in member_list:
+        # 周回数 check
+        if member.equal_ra(ra_value)  == False:
+            continue
+        total += int(member.damage_str)
+    return total
+
+# 周回数をintで返す
+def get_roundabout(wks):
+    val = wks.acell(cell_ROUNDABOUT).value
+    val = re.sub(r'\D', '',val) # d only
+    return int(val) # type int
+
+# 周回数をsetする
+def set_roundabout(wks,round_num):
+    wks.update_acell(cell_ROUNDABOUT,str(round_num)+"周目")
+
+# wksを返す
+def setup(sheet_name):
+    sheet_name=sheet_name.replace(" ", "")
+
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(path+'gspread/gspread-sample-2bf9fcc59d37.json', scope)
+    gc = gspread.authorize(credentials)
+    try:
+        workbook=gc.open_by_url(WORKBOOK_URL)
+        worksheet_list = workbook.worksheets()
+    except:
+        return None
+    return search_sheet(worksheet_list,sheet_name)
+
+# 
+def _next_attack_member(wks):
+    #周回数
+    #cell_ra_value = get_roundabout(wks)
+    #メンバー取得
+    member_list = create_member_list(wks)
+    r_list = []
+    ra_value = get_roundabout(wks)
+    for member in member_list:
+        # 周回数 check
+        if member.equal_ra(ra_value)  == False:
+            continue
+        r_list.append(member.get_string())
+
+    return r_list
+
+# -1 not found boss
+# -2 not found user
+# -3 not reserve user   
+# userの攻撃情報更新
+def upd_attack_member_cell(wks,user_name,in_damage_cell,in_ra_cell):
+    # ユーザーがいるかチェック
+    cell_user_list = wks.range(cell_USERLIST)
+    user_cell = None
+    for cell in cell_user_list:
+        if user_name in cell.value:
+            user_cell = cell
+
+    # 名前一致ユーザーなし
+    if user_cell is None:
+        return -2
+
+    is_reserve = True
+    if in_damage_cell == 0:
+        is_reserve = False
+
+    col_sp_offset = 4 #sp1
+    col_ra_offset = 7 #round about
+
+    # 予約をいれるならhp check
+    if is_reserve == True:
+        # boss hp 取得
+        boss_hp = int(wks.acell(cell_BOSS_HP).value)
+        # トータルダメージの計算
+        total_damage = calc_total_reserve_damage(wks,in_ra_cell)
+        if boss_hp < total_damage:
+            return -3
+    else:
+        cell = wks.cell(user_cell.row,user_cell.col+col_sp_offset)
+        if len(cell.value) <= 0:
+            return -3
+
+    # 登録/解除
+    if is_reserve:
+        wks.update_cell(user_cell.row,user_cell.col+col_sp_offset,in_damage_cell)
+    else:
+        wks.update_cell(user_cell.row,user_cell.col+col_sp_offset,"")
+
+    wks.update_cell(user_cell.row,user_cell.col+col_ra_offset,in_ra_cell)
+    return 0
+
+# fin_round周目の攻撃情報を消す
+def clear_round_member_cell(wks,fin_round):
+
+    cell_list = wks.range(cell_RESERVELIST)
+    c=4
+    r_member_list = []
+    upd_cells = []
+    for i in range(0,30):
+        sp_cell = cell_list[0+i*c]
+        sp2_cell = cell_list[1+i*c]
+        remark_cell = cell_list[2+i*c]
+        round_num_cell = cell_list[3+i*c]
+        if round_num_cell.value == "":
+            continue
+        if int(round_num_cell.value) != fin_round:
+            continue
+
+        # cell clear
+        sp_cell.value = ""
+        upd_cells.append(sp_cell)
+        sp2_cell.value = ""
+        upd_cells.append(sp2_cell)
+        remark_cell.value = ""
+        upd_cells.append(remark_cell)
+        round_num_cell.value = ""
+        upd_cells.append(round_num_cell)
+    
+        # 消したuserをlistで返す
+        offset_name=-4
+        name_cell = wks.cell(sp_cell.row,sp_cell.col+offset_name)
+        r_member_list.append(name_cell.value)
+        
+    # bach update
+    if len(upd_cells) > 0:
+        wks.update_cells(upd_cells)
+    return r_member_list
+
+#================================
+# api func
+
+# 次回攻撃メンバーを配列で返す
+def next_attack_member(boss_name):
+    wks = setup(boss_name)
+    if(wks == None):
+        print("not found wks["+boss_name+"]")
+        return None
+
+    return _next_attack_member(wks)
+
+# -1 not found boss
+# -2 not found user
+# -3 not reserve user
+def reserve_attack_member(boss_name,user_name,damage):
+    wks = setup(boss_name)
+    if(wks == None):
+        print("not found wks["+boss_name+"]")
+        return -1
+
+    #周回数
+    cell_ra_value = get_roundabout(wks)
+    result = upd_attack_member_cell(wks,user_name,damage,cell_ra_value)
+    if result != 0:
+        return result
+    
+    return _next_attack_member(wks)
+
+# -1 not found boss
+# -2 not found user
+# -3 not reserve user
+def cancel_attack_member(boss_name,user_name):
+    wks = setup(boss_name)
+    if(wks == None):
+        print("not found wks["+boss_name+"]")
+        return -1
+    # cancel
+    result = upd_attack_member_cell(wks,user_name,0,"")
+    if result != 0:
+        return result
+    
+    return 0
+    #_next_attack_member(wks)
+
+
+# -1 not found boss
+# 周回数の情報を消し、消した周回数が現在の周回数と一致するなら+1する
+def clear_round_and_countup(boss_name,fin_round):
+    wks = setup(boss_name)
+    if(wks == None):
+        print("not found wks["+boss_name+"]")
+        return [[],None]
+    # clear
+    result = clear_round_member_cell(wks,int(fin_round))
+    # 周回数が一致しているなら+1する
+    next_round = int(fin_round)+1
+    if get_roundabout(wks) == int(fin_round):
+        set_roundabout(wks,next_round)
+        return [result,next_round]
+
+    return [result,None]
+
+# -2
+# -1
+# 指定PTの凸記入をする
+def upd_pt_convex(user_name,pt_num):
+    wks = setup(PT_CONVEX_SHEET_NAME)
+    if(wks == None):
+        print("not found wks["+PT_CONVEX_SHEET_NAME+"]")
+        return -1
+
+    # ユーザーがいるかチェック
+    cell_user_list = wks.range(cell_PT_CONVEX_USERLIST)
+    user_cell = None
+    for cell in cell_user_list:
+        if user_name in cell.value:
+            user_cell = cell
+
+    # 名前一致ユーザーなし
+    if user_cell is None:
+        return -2
+    
+    col_offset = 2+int(pt_num)
+    wks.update_cell(user_cell.row,user_cell.col+col_offset,"PT"+str(pt_num))
+    return int(pt_num)
+
+#クラバトurlを返す
+def get_url():
+    return WORKBOOK_URL
+
+# settingをロードする
+def load_setting_url():
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(path+'gspread/gspread-sample-2bf9fcc59d37.json', scope)
+    gc = gspread.authorize(credentials)
+    try:
+        workbook=gc.open_by_url(SETTINGS_URL)
+        worksheet_list = workbook.worksheets()
+    except:
+        return None
+    wks = search_sheet(worksheet_list,SETTINGS_SHEET_NAME)
+    load_env_list = wks.range('A1:B'+str(SETTINGS_ENV_LIMIT))
+    c=2
+
+    global env_list
+    for i in range(0,SETTINGS_ENV_LIMIT):
+        env_name = load_env_list[i*c].value
+        env_data = load_env_list[1+i*c].value
+        if env_name == "":
+            continue
+        print(env_name)
+        env_list[env_name]=env_data
+
+# settingをsaveする
+def save_setting_url(new_env_list:dict):
+    if new_env_list is None:
+        return
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(path+'gspread/gspread-sample-2bf9fcc59d37.json', scope)
+    gc = gspread.authorize(credentials)
+    try:
+        workbook=gc.open_by_url(SETTINGS_URL)
+        worksheet_list = workbook.worksheets()
+    except:
+        return
+    wks = search_sheet(worksheet_list,SETTINGS_SHEET_NAME)
+
+    count=1
+    for k,v in new_env_list.items():
+        wks.update_cell(count,1,k)
+        wks.update_cell(count,2,v)
+        count+=1
+
+#  クラバトurlをリロードする
+def reload_url():
+    load_setting_url()
+    if env_list["clanbattle_url"] is None:
+        return None
+
+
+    global WORKBOOK_URL
+    old = WORKBOOK_URL
+    WORKBOOK_URL = env_list["clanbattle_url"]
+    return [old,WORKBOOK_URL]
+
+def get_env_list():
+    load_setting_url()
+    global env_list
+    return env_list
+
+def main():
+    load_setting_url()
+    global env_list
+    print(json.dumps(env_list))
+    env_list["chat_channel_name"]=1222
+    save_setting_url(env_list)
+
+if __name__ == "__main__":
+    main()
+else:
+    f=open(path+"workbook","r",encoding="utf-8")
+    SETTINGS_URL=f.read().replace('\n','')
+    reload_url()
+
